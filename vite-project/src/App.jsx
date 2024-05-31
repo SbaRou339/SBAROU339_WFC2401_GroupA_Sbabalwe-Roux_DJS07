629f071da111337363fcf0b0f152fab1085bd31a
SbabalweRoux <sbaviaroux@gmail.com>
2024-05-31 16:24:30 +0200

    Fixed the styling in the styling of the page

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import Meme from "./meme";
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
+
